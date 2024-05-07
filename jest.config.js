15775018447c04aa415aa40782be5155105339c0
Rian Fiore <rian.camara@eumedicoresidente.com.br>
2024-05-07 08:53:43 -0300

    fix: reconfig jest, packages, tsconfig after much errors

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
-import type { Config } from "jest";
-import nextJest from "next/jest.js";
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -7,18 +6,20 @@ const createJestConfig = nextJest({
 });
 
 // Add any custom config to be passed to Jest
-const config: Config = {
-  coverageProvider: "v8",
-  testEnvironment: "jest-environment-jsdom",
+const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  preset: "ts-jest",
+  testEnvironment: "jsdom",
+
   collectCoverageFrom: [
     "src/app/{components,hooks}/**/*.tsx",
     "src/app/utils/**/*.ts",
     "!src/app/utils/index.ts",
     "!src/app/icons/**",
   ],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/app/$1",
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config);
+module.exports = createJestConfig(customJestConfig);
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,29 @@
+// Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+const originalConsoleError = console.error;
+const originalConsoleWarning = console.warn;
+
+// Esse erro é um problema de incompatibilidade entre as versões do Next, React e Jest. Muitas pessoas estão reclamando desse mesmo problema, e não encontrei nenhuma solução definitiva. Então, criei isso para ignorar a mensagem de erro.
+beforeEach(() => {
+  console.error = (message) => {
+
+    if (
+      !message.includes("React does not recognize the `fetchPriority` prop")
+    ) {
+      console.error = jest.fn();
+    }
+  };
+
+  console.warn = (message) => {
+    if (!message.includes("Warning: Prop `className` did not match")) {
+      console.warn = jest.fn();
+    }
+  };
+});
+
+// Após cada teste, restaura console.error original
+afterEach(() => {
+  console.error = originalConsoleError;
+  console.warn = originalConsoleWarning;
+});
