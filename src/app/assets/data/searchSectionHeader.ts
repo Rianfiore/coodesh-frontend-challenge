61ade2d6751f829df99040be2cd5cdc67c00c559
Rian Fiore <rian.camara@eumedicoresidente.com.br>
2024-05-10 20:11:03 -0300

    chore(assets): moving all data files to the app/assets folder

---
+++ src/app/assets/data/searchSectionHeader.ts
@@ -0,0 +1,18 @@
+type SearchSectionHeaderData = {
+  id: number;
+  title: string;
+  isSelected: boolean;
+};
+
+export const searchSectionHeaderData: SearchSectionHeaderData[] = [
+  {
+    id: 1,
+    title: "Alugar",
+    isSelected: true,
+  },
+  {
+    id: 2,
+    title: "Comprar",
+    isSelected: false,
+  },
+];
