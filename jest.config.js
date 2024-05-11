8210ada5f037feb2f7b224e2f74ada80e8f621fa
Rian Fiore <rian.camara@eumedicoresidente.com.br>
2024-05-11 15:23:00 -0300

    chore(jest.config.js): update Jest configuration for swiper lib

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,6 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
-
   collectCoverageFrom: [
     "src/app/**/*.tsx",
     "src/app/**/*.test.tsx",
@@ -21,7 +20,17 @@ const customJestConfig = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/app/$1",
     "^@/assets/(.*)$": "<rootDir>/public/$1",
+    "^swiper": "<rootDir>/node_modules/swiper/swiper.min.js",
+    "swiper/react": "swiper/swiper-react.d.ts",
+    "swiper/modules": "swiper/types/modules/index.d.ts",
+    "swiper/css": "swiper/swiper.min.css",
   },
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/.next/",
+    "src/app/(pages)/(publicRoutes)/(home)/Home.test.tsx",
+  ],
+  transformIgnorePatterns: ["/node_modules/(?!swiper|ssr-window|dom7)"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
